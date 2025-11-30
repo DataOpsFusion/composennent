5f31f0eb5d44429e3e1722723077c49cef4c6824
DataOpsFusion <tad9277@gmail.com>
2025-11-30 18:09:36 -0500

    remove uncreated import

--- src/composennent/training/__init__.py
+++ src/composennent/training/__init__.py
@@ -1,8 +1,6 @@
 """Training utilities including dataloader, optimizer, and learning rate schedules."""
 
 from .dataloader import DataLoader
-from .optim import Optimizer
-from .schedule import Schedule
 from .trainer import Trainer
 
-__all__ = ["DataLoader", "Optimizer", "Schedule", "Trainer"]
+__all__ = ["DataLoader", "Trainer"]
